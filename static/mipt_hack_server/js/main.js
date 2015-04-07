f994ade2bff4750531715a261b0b45485b1c0d1b
Igor Stepanov <sigorilla@gmail.com>
2015-04-08 01:45:01 +0300

    main page with emails

--- static/mipt_hack_server/js/main.js
+++ static/mipt_hack_server/js/main.js
@@ -1,18 +1,22 @@
 var HACK;
 
-$( document ).ready( function() {
+$( document ).ready( function () {
 
-	HACK = ( function() {
+	HACK = ( function () {
 
-		var _init = function() {
+		var $loginForm = $( "#loginForm" );
+
+		var regEmail = /.+@.+\..+/i;
+
+		var _init = function () {
 			console.log( "HACK is running." );
 
 			_addListener();
 		};
 
-		var _addListener = function() {
+		var _addListener = function () {
 
-			$( "[data-fire]" ).click( function() {
+			$( "[data-fire]" ).click( function () {
 				var fire = $( this ).data( "fire" );
 				if ( fire == "totop" ) {
 					$( "html, body" ).animate( { scrollTop: 0 } );
@@ -20,6 +24,38 @@ $( document ).ready( function() {
 				if ( fire == "print" ) {
 					window.print();
 				}
+			} );
+
+			$loginForm.find( "[data-type=submit]" ).click( function ( event ) {
+				event.preventDefault();
+
+				var checkin = true;
+
+				$( this ).parent( ".form-group" ).removeClass( "has-error has-success" );
+				var $email = $( this ).parents( "#loginForm" ).find( "input[name=email]" );
+				var $password = $( this ).parents( "#loginForm" ).find( "input[name=password]" );
+
+				if ( $email !== undefined ) {
+					if ( !regEmail.test( $email.val() ) ) {
+						$email.parent( ".form-group" ).addClass( "has-error" );
+					} else {
+						$email.parent( ".form-group" ).addClass( "has-success" );
+					}
+				}
+
+				if ( $password.val().length == 0 ) {
+					$password.parent( ".form-group" ).addClass( "has-error" );
+				} else {
+					$password.parent( ".form-group" ).addClass( "has-success" );
+				}
+
+				if ( $loginForm.find( ".form-group.has-success" ).length == 2 ) {
+					$loginForm.submit();
+				}
+			} );
+
+			$loginForm.find( "input" ).on( "focus", function ( event ) {
+				$( this ).parent( ".form-group" ).removeClass( "has-error" );
 			} );
 
 		};
--- views.py
+++ views.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.core.mail import send_mail, EmailMessage
 
 def index(request):
-  return render(request, 'mipt_hack_server/index.html')
+	obj = dict()
+	if (request.method == "POST"):
+		email = request.POST["email"]
+		try:
+			send = send_mail("Test reg", "Hello!", 'sigorilla@gmail.com', [email])
+		except Exception, e:
+			send = False
+		obj = {
+			'email': send
+		}
+	return render(request, "mipt_hack_server/index.html", obj)
