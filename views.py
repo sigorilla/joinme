19458cde036f727edd37d6f8d6472d8a28ccbd10
Igor Stepanov <sigorilla@gmail.com>
2015-04-11 01:04:01 +0300

    add searching

--- views.py
+++ views.py
@@ -6,12 +6,49 @@ from django.shortcuts import render, render_to_response, get_object_or_404
 from django.core.mail import send_mail
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.core.urlresolvers import *
 from django.http import HttpResponseRedirect
 from django.views import generic
 
 from mipt_hack_server.models import UserProfile, Category, Event, User
 from mipt_hack_server.forms import RegistrationForm, ResetForm
+
+def normalize_query(query_string,
+					findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+					normspace=re.compile(r'\s{2,}').sub):
+	''' Splits the query string in invidual keywords, getting rid of unecessary spaces
+		and grouping quoted words together.
+		Example:
+		
+		>>> normalize_query('  some random  words "with   quotes  " and   spaces')
+		['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+	
+	'''
+	return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
+
+def get_query(query_string, search_fields):
+	''' Returns a query, that is a combination of Q objects. That combination
+		aims to search keywords within a model by testing the given search fields.
+	
+	'''
+	# Query to search for every search term
+	query = None
+	terms = normalize_query(query_string)
+	for term in terms:
+		# Query to search for a given term in each field
+		or_query = None 
+		for field_name in search_fields:
+			q = Q(**{"%s__icontains" % field_name: term})
+			if or_query is None:
+				or_query = q
+			else:
+				or_query = or_query | q
+		if query is None:
+			query = or_query
+		else:
+			query = query & or_query
+	return query
 
 class LoginRequiredMixin(object):
 	@classmethod
@@ -145,3 +182,14 @@ class MyEventsList(LoginRequiredMixin, generic.ListView):
 	def get_queryset(self):
 		author = UserProfile.objects.filter(user__id=self.request.user.id).values()[0]
 		return Event.objects.filter(author__id=author["id"])
+
+def search(request):
+	query_string = ''
+	found_entries = None
+	if ('q' in request.GET) and request.GET['q'].strip():
+		query_string = request.GET['q']
+		entry_query = get_query(query_string, ['title', 'description',])
+		found_entries = Event.objects.filter(entry_query).order_by('-pub_date')
+
+	return render(request, 'mipt_hack_server/search.html',
+						{ 'query': query_string, 'results': found_entries })
