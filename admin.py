6eb5e4d8acf3800f919bed9b815f090156ca4fc8
Igor Stepanov <sigorilla@gmail.com>
2015-04-10 15:17:16 +0300

    add models of catogories and events

--- admin.py
+++ admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from mipt_hack_server.models import UserProfile
+from mipt_hack_server.models import UserProfile, Category, Event
 
 admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(Event)
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Count
+from django.utils.timezone import datetime as dt
 
 class UserProfile(models.Model):
 
@@ -16,3 +18,40 @@ class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	activation_key = models.CharField(max_length=40)
 	key_expires = models.DateTimeField()
+
+class Category(models.Model):
+
+	class Meta:
+		verbose_name = "Category"
+		verbose_name_plural = "Categories"
+
+	def __str__(self):
+		return self.title
+
+	def __unicode__(self):
+		return self.title
+
+	title = models.CharField(max_length=200)
+	description = models.TextField(blank=True)
+	icon = models.CharField(max_length=20, blank=True)
+
+class Event(models.Model):
+
+	class Meta:
+		verbose_name = "Event"
+		verbose_name_plural = "Events"
+
+	def __str__(self):
+		return self.title
+
+	def __unicode__(self):
+		return self.title
+
+	title = models.CharField(max_length=200)
+	description = models.TextField()
+	datetime = models.DateTimeField()
+	pub_date = models.DateTimeField("date published", default=dt.now())
+	count_users = models.IntegerField(blank=True, default=100)
+	category = models.ForeignKey(Category)
+	author = models.ForeignKey(UserProfile, related_name='author')
+	users = models.ManyToManyField(UserProfile, blank=True)
