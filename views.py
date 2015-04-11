a2d8db3ae559229a9d7a7b6ad096973f53455cdb
Igor Stepanov <sigorilla@gmail.com>
2015-04-11 17:53:05 +0300

    Add masonry and paginator

--- views.py
+++ views.py
@@ -14,42 +14,6 @@ from django.views import generic
 from mipt_hack_server.models import UserProfile, Category, Event, User
 from mipt_hack_server.forms import RegistrationForm, ResetForm, PasswordResetForm
 
-def normalize_query(query_string,
-					findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
-					normspace=re.compile(r'\s{2,}').sub):
-	''' Splits the query string in invidual keywords, getting rid of unecessary spaces
-		and grouping quoted words together.
-		Example:
-		
-		>>> normalize_query('  some random  words "with   quotes  " and   spaces')
-		['some', 'random', 'words', 'with quotes', 'and', 'spaces']
-	
-	'''
-	return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
-
-def get_query(query_string, search_fields):
-	''' Returns a query, that is a combination of Q objects. That combination
-		aims to search keywords within a model by testing the given search fields.
-	
-	'''
-	# Query to search for every search term
-	query = None
-	terms = normalize_query(query_string)
-	for term in terms:
-		# Query to search for a given term in each field
-		or_query = None 
-		for field_name in search_fields:
-			q = Q(**{"%s__icontains" % field_name: term})
-			if or_query is None:
-				or_query = q
-			else:
-				or_query = or_query | q
-		if query is None:
-			query = or_query
-		else:
-			query = query | or_query
-	return query
-
 class LoginRequiredMixin(object):
 	@classmethod
 	def as_view(cls, **initkwargs):
@@ -174,10 +138,12 @@ class CategoryView(LoginRequiredMixin, generic.DetailView):
 class EventView(LoginRequiredMixin, generic.DetailView):
 	model = Event
 	template_name = "mipt_hack_server/event.html"
+	paginate_by = 10
 
 class MyEventsList(LoginRequiredMixin, generic.ListView):
 	template_name = "mipt_hack_server/myevents.html"
 	context_object_name = "events"
+	paginate_by = 10
 
 	def get_queryset(self):
 		author = UserProfile.objects.filter(user__id=self.request.user.id).values()[0]
@@ -186,6 +152,7 @@ class MyEventsList(LoginRequiredMixin, generic.ListView):
 class AllEventsList(LoginRequiredMixin, generic.ListView):
 	template_name = "mipt_hack_server/allevents.html"
 	context_object_name = "events"
+	paginate_by = 10
 
 	def get_queryset(self):
 		return Event.objects.filter(active__exact=True).order_by('-pub_date')
@@ -213,13 +180,54 @@ class ResetPassword(generic.FormView):
 		else:
 			return self.form_invalid(form)
 
-def search(request):
-	query_string = ''
-	found_entries = None
-	if ('q' in request.GET) and request.GET['q'].strip():
-		query_string = request.GET['q']
-		entry_query = get_query(query_string, ['title', 'description',])
-		found_entries = Event.objects.filter(entry_query).order_by('-pub_date')
+class SearchList(generic.ListView):
+	model = Event
+	template_name = "mipt_hack_server/search.html"
+	paginate_by = 10
 
-	return render(request, 'mipt_hack_server/search.html',
-						{ 'query': query_string, 'results': found_entries })
+	def normalize_query(self,
+						query_string,
+						findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+						normspace=re.compile(r'\s{2,}').sub):
+		''' Splits the query string in individual keywords, getting rid of unecessary spaces
+			and grouping quoted words together.
+			Example:
+			>>> normalize_query('  some random  words "with   quotes  " and   spaces')
+			['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+		'''
+		return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
+
+	def get_query(self, query_string, search_fields):
+		''' Returns a query, that is a combination of Q objects. That combination
+			aims to search keywords within a model by testing the given search fields.
+		'''
+		# Query to search for every search term
+		query = None
+		terms = self.normalize_query(query_string)
+		for term in terms:
+			# Query to search for a given term in each field
+			or_query = None 
+			for field_name in search_fields:
+				q = Q(**{"%s__icontains" % field_name: term})
+				if or_query is None:
+					or_query = q
+				else:
+					or_query = or_query | q
+			if query is None:
+				query = or_query
+			else:
+				query = query | or_query
+		return query
+
+	def get_queryset(self):
+		query_string = ''
+		found_entries = None
+		if ('q' in self.request.GET) and self.request.GET['q'].strip():
+			query_string = self.request.GET['q']
+			entry_query = self.get_query(query_string, ['title', 'description',])
+			return Event.objects.filter(entry_query).order_by('-pub_date')
+
+	def get_context_data(self, **kwargs):
+		context = super(SearchList, self).get_context_data(**kwargs)
+		context["query"] = self.request.GET['q']
+		return context
