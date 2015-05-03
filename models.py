e9ffd336e1ca787db35c95f5cdbb782a404b0339
Igor Stepanov <sigorilla@gmail.com>
2015-05-03 16:58:27 +0300

    Model: order to comments

--- models.py
+++ models.py
@@ -66,6 +66,7 @@ class Comment(models.Model):
     class Meta:
         verbose_name = "Комментарий"
         verbose_name_plural = "Комментарии"
+        ordering = ['pub_date']
 
     def __str__(self):
         return self.message
